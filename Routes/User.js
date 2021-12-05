264248f02f82dc0a8d6ad545aaae7d5795b487a9
Sameer Badami <sameerbadami.sb@gmail.com>
2021-12-05 19:53:26 +0530

    New changes

--- Routes/User.js
+++ Routes/User.js
@@ -16,18 +16,4 @@ router.get("/Posts", function (req, res) {
     })
 })
 
-router.get("/Posts", function (req, res) {
-
-    con.query("Select * from tbusers where UserID=?", [0], (err, result) => {
-        if (err) throw err;
-        if (result.length > 0) {
-            res.send({ "count": "non zero" })
-        }
-        else {
-            res.send({ "count": "zero" })
-        }
-    })
-})
-
-
 module.exports = router;
